577fc2da5d8fd67623d7bec923cac78e36f802ab
Melikk <aramelik@gmail.com>
2024-08-21 21:51:59 +0600

    Initial commit

---
+++ src/main/java/com/example/sessionpaymentapi/SessionPaymentApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sessionpaymentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionPaymentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionPaymentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sessionpaymentapi/SessionPaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sessionpaymentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionPaymentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
